f987d9b02b6d10d146e2913acb6f80bd4f9c2ac4
ravikirananaparthi <anaparthiravikiran20037@gmail.com>
2026-01-02 22:49:50 +0530

    feat: update carousel exports

--- src/components/carousel/index.ts
+++ src/components/carousel/index.ts
@@ -2,6 +2,7 @@
 export { BottomBar } from './BottomBar';
 export { CarouselItem } from './CarouselItem';
 export { CarouselScreen } from './CarouselScreen';
+export { MenuDropdown } from './MenuDropdown';
 export { TopBar } from './TopBar';
 export { ZoomableImage } from './ZoomableImage';
 
